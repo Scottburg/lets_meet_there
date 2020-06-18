97da23fc18ed238ed2ae44569ad7ca7c4b3c690d
Andrew Cooke <cookecodes89@gmail.com>
2020-06-18 16:42:15 +0200

    Testing: carrierReducer tests complete

--- src/Reducers/carriersReducer.js
+++ src/Reducers/carriersReducer.js
@@ -3,6 +3,9 @@ import helpers from '../helpers';
 const carriersReducer = (state = {}, action) => {
   switch (action.type) {
     case 'home/getCarriers':
+      console.log('quotesa',action.quotesA)
+      console.log('quotesb',action.quotesB)
+      console.log(helpers.createDict(action.quotesA, action.quotesB, 'CarrierId'))
       return helpers.createDict(action.quotesA, action.quotesB, 'CarrierId');
     default:
       return state;
