99e237a95b7c5506e24898390498cbc167cd8b97
scottburg <scottb4545@gmail.com>
2020-09-07 12:26:39 +0100

    TEST: Add testing to App(Home Screen)

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const mockStore = configureStore([]);
+const store = mockStore();
+
+describe('test the Spinner', () => {
+  it('should match snapshot', () => {
+    const { asFragment } = render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+    expect(asFragment(<App />)).toMatchSnapshot();
+  });
+
+  it('should render correctly', () => {
+    const { getByText } = render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+    const title = getByText('Search for a place to meet');
+    expect(title).toBeInTheDocument();
+  });
 });
