64f148f5f31722ca39d775df7c0da7787d463d8b
scottburg <scottb4545@gmail.com>
2020-09-16 16:32:27 +0100

    FEAT: tests added to all remaining components

---
+++ src/Components/SearchForm/searchForm.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import { configure, shallow } from 'enzyme';
+import SearchForm from './searchForm';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
+it('render correctly text component', () => {
+  const searchForm = renderer.create(<SearchForm />).toJSON();
+  expect(searchForm).toMatchSnapshot();
+});
+
+it('display correct text', () => {
+  const searchForm = render(<SearchForm />);
+  searchForm.findAllByText('Lets Meet There').then((data) => data);
+});
+
+it('should have an two text fields', () => {
+  const searchForm = shallow(<SearchForm />);
+  expect(searchForm.find('input[type="text"]').length).toEqual(2);
+});
+
+it('input fields should have correct props', () => {
+  const searchForm = shallow(<SearchForm />);
+  expect(searchForm.find('input[name="from1"]').props()).toEqual({
+    onChange: expect.any(Function),
+    type: 'text',
+    className: 'inputboxes',
+    name: 'from1',
+    placeholder: 'E.g. London',
+    value: '',
+  });
+
+  expect(searchForm.find('input[name="from2"]').props()).toEqual({
+    onChange: expect.any(Function),
+    type: 'text',
+    className: 'inputboxes',
+    name: 'from2',
+    placeholder: 'E.g. Barcelona',
+    value: '',
+  });
+});
---
+++ src/Mocks/data.mock.js
@@ -0,0 +1,128 @@
+export const placeMocks = {
+    65698: {
+      CityId: "LOND",
+      CityName: "London",
+      CountryName: "United Kingdom",
+      IataCode: "LHR",
+      Name: "London Heathrow",
+      PlaceId: 65698,
+      SkyscannerCode: "LHR",
+      Type: "Station"
+    }
+  };
+
+export const quotes = [{
+  QuoteId: 1,
+  MinPrice: 349,
+  Direct: false,
+  OutboundLeg: {
+    CarrierIds: [
+      1090
+    ],
+    OriginId: 42414,
+    DestinationId: 81495,
+    DepartureDate: '2020-06-26T00:00:00'
+  },
+  InboundLeg: {
+    CarrierIds: [
+      1755
+    ],
+    OriginId: 81495,
+    DestinationId: 42414,
+    DepartureDate: '2020-06-29T00:00:00'
+  },
+  QuoteDateTime: '2020-06-17T21:12:00'
+}];
+
+export const carriers = [{
+
+}];
+
+export const flightOne = {
+  Direct: false,
+  InboundLeg: {
+    CarrierIds: [1324],
+    DepartureDate: "2020-06-27T00:00:00",
+    DestinationId: 1165698,
+    OriginId: 40920,
+  },
+  MinPrice: 213,
+  OutboundLeg: {
+    CarrierIds: [1324],
+    DepartureDate: "2020-06-25T00:00:00",
+    DestinationId: 40920,
+    OriginId: 65698,
+  },
+  QuoteDateTime: "2020-06-17T23:03:00",
+  QuoteId: 19
+}
+
+export const flightTwo = {
+  Direct: false,
+  InboundLeg: {
+    CarrierIds: [1324],
+    DepartureDate: "2020-06-27T00:00:00",
+    DestinationId: 42414,
+    OriginId: 40920
+  },
+  MinPrice: 192,
+  OutboundLeg: {
+    CarrierIds: [1324],
+    DepartureDate: "2020-06-25T00:00:00",
+    DestinationId: 40920,
+    OriginId: 65698
+  },
+  QuoteDateTime: "2020-06-15T19:17:00",
+  QuoteId: 10
+}
+
+export const flightListMock = [[
+  66328,
+  [
+    {
+      QuoteId: 59,
+      MinPrice: 177,
+      Direct: false,
+      OutboundLeg: {
+        CarrierIds: [
+          1090
+        ],
+        OriginId: 82398,
+        DestinationId: 66328,
+        DepartureDate: '2020-06-26T00:00:00'
+      },
+      InboundLeg: {
+        CarrierIds: [
+          1090
+        ],
+        OriginId: 66328,
+        DestinationId: 82398,
+        DepartureDate: '2020-06-29T00:00:00'
+      },
+      QuoteDateTime: '2020-06-18T10:10:00'
+    }
+  ], [
+    {
+      QuoteId: 9,
+      MinPrice: 26,
+      Direct: true,
+      OutboundLeg: {
+        CarrierIds: [
+          1090
+        ],
+        OriginId: 42414,
+        DestinationId: 66328,
+        DepartureDate: '2020-06-26T00:00:00'
+      },
+      InboundLeg: {
+        CarrierIds: [
+          1090
+        ],
+        OriginId: 66328,
+        DestinationId: 42414,
+        DepartureDate: '2020-06-29T00:00:00'
+      },
+      QuoteDateTime: '2020-06-18T16:38:00'
+    }
+  ]
+]];
